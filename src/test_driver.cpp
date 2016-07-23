1c66de447e4b320b0e9c48c453c868427c593220
Andrew Faulds <a.faulds@verizon.net>
2016-07-22 22:17:40 -0700

    Add catch unit test framework

---
+++ src/test_driver.cpp
@@ -0,0 +1,6 @@
+// This file is the main driver for all unit tests. It
+// is the only one that #defines CATCH_CONFIG_MAIN and
+// thus contains the main block for running Catch tests.
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
